f1570bb6a476139c4a655bfe5fb93e919e422d9f
Michael Cuffaro <consulting@michaelcuffaro.com>
2026-07-05 10:05:30 -0400

    remove intermediate crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,53 @@
-use rltbl_db::db_value::DbRow;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::DeriveInput;
 
-pub trait ConvertDbRow {
-    fn into_db_row(&self) -> DbRow;
-    fn from_db_row(value: DbRow) -> Self;
+#[proc_macro_derive(ConvertDbRow)]
+pub fn convert_db_row_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate.
+    let ast = syn::parse(input).expect("Could not parse input token stream");
+
+    // Build the trait implementation.
+    impl_convert_db_row(&ast)
+}
+
+fn impl_convert_db_row(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+
+    println!("DATA: {:#?}", ast.data);
+
+    // TODO: Look at the code for rust's Clone macro for hints.
+
+    let fields = match &ast.data {
+        syn::Data::Struct(data_struct) => &data_struct.fields,
+        _ => panic!("Invalid data"),
+    };
+    let fields = match fields {
+        syn::Fields::Named(fields) => fields,
+        _ => panic!("Invalid fields"),
+    };
+
+    let mut field_names = vec![];
+    for field in fields.named.iter() {
+        let field_ident = field.ident.clone().unwrap();
+        println!("FIELD IDENT: {field_ident:?}");
+        field_names.push(field_ident.to_string());
+    }
+    println!("FIELD_NAMES: {field_names:#?}");
+
+    let generated = quote! {
+        impl Into<DbRow> for #name {
+            fn into(self) -> DbRow {
+                DbRow::new()
+            }
+        }
+
+        impl From<DbRow> for #name {
+            fn from(value: DbRow) -> Self {
+                rltbl_db::serde::from_db_row(&value).unwrap()
+            }
+        }
+    };
+    generated.into()
 }
